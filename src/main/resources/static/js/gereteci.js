fb58f677e697e33f66c9644533164fc58226ba0f
Jean <Jean>
2016-01-21 10:45:11 -0300

    CRUD de computadores aparentemente completo

--- src/main/resources/static/js/gereteci.js
+++ src/main/resources/static/js/gereteci.js
@@ -17,3 +17,23 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event)
 	
 	
 });
+
+$('#confirmacaoExclusaoModalComputador').on('show.bs.modal', function(event)
+		{
+			var button = $(event.relatedTarget);
+			var codigoComputador = button.data('id_computador');
+			var ipComputador = button.data('ip');
+			
+			var modal = $(this);
+			var form = modal.find('form');
+			var action = form.data('url-base');
+			if(!action.endsWith('/'))
+				{
+					action += '/';
+				}
+			
+				form.attr('action', action + codigoComputador);
+				modal.find('.modal-body span').html('Tem certeza que deseja excluir o conmputador de IP: <strong>' + ipComputador + '</strong>?');
+			
+			
+		});
