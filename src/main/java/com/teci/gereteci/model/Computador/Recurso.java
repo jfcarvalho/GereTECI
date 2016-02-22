13ee04fa9a661f33bedf2f060cca63a325b65edf
Jean <Jean>
2016-02-22 16:56:52 -0300

    Enviando atualizacoes

--- src/main/java/com/teci/gereteci/model/Computador/Recurso.java
+++ src/main/java/com/teci/gereteci/model/Computador/Recurso.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 
 @Entity
